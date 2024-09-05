9902b8afad9203ba10f9ea010300ec46923c54b9
PennyH0W <160952834+PennyH0W@users.noreply.github.com>
2024-09-05 08:35:35 -0400

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@
 const footer = document.createElement('footer');
 footer.innerHTML = `
     <p>&copy; 2024 Penny Howard. All Rights Reserved.</p>
-    <p><strong:>Website:</strong><a href="https://sitespyder.com/">SiteSpyder.com</a></p>
+    <p><strong:>Website: </strong><a href="https://sitespyder.com/">SiteSpyder.com</a></p>
 `;
 
 document.body.appendChild(footer);
